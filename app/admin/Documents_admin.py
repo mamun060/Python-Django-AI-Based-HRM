e8f08d052ddb352cd6a677e4cd10e56f362e9f34
mamun060 <almamun.raju520@gmail.com>
2025-07-07 23:16:49 +0600

    Employee docs file view from the admin

--- app/admin/Documents_admin.py
+++ app/admin/Documents_admin.py
@@ -13,3 +13,15 @@ class DocumentAdmin(ModelAdmin):
 #     list_display = ('filename' , 'filedescription' , 'file')
 #     search_fields = ('filename', )
 # admin.site.register(Document, DocumentsAdmin)
+
+from django.utils.html import format_html
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_number', 'pdf_link')
+
+    def pdf_link(self, obj):
+        file_name = obj.generated_pdf_name  # Example: Invoice_-_SERINV00000005.pdf
+        url = f"/officialDocs/{file_name}"
+        return format_html('<a href="{}" target="_blank">View Invoice</a>', url)
+
+    pdf_link.short_description = "Invoice PDF"
--- hrm/settings.py
+++ hrm/settings.py
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
 MDDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
